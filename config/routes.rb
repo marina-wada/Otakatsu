a14ccc35d331083c8d743c5d78754ddb1646eb81
marina-wada <wdmrna4i1@gmail.com>
2021-09-05 14:16:39 +0000

    [Add]homes_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :user do
+    get 'homes/top'
+  end
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
@@ -6,5 +9,6 @@ Rails.application.routes.draw do
   devise_for :admins, controllers: {
     sessions: 'admins/sessions'
   }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
