ac7d3c4c7b785e5d61a1f6aa9e99631373c1b205
marina-wada <wdmrna4i1@gmail.com>
2021-09-09 08:44:34 +0000

    [Add]DM

--- app/controllers/user/rooms_controller.rb
+++ app/controllers/user/rooms_controller.rb
@@ -1,2 +1,21 @@
 class User::RoomsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @room = Room.create
+    @entry1 = Entry.create(room_id, @room_id, user_id: current_user.id)
+    @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id))
+    redirect_to room_path(@room.id)
+  end
+
+  def show
+    @room = Room.find(params[:id])
+    if Entry.where(user_id: current_user.id, room_id: @room.id).present?
+      @messages = @room.messages
+      @message = Message.new
+      @entries = @room.entries
+    else
+      redirect_back(fallback_location: item_path(@item))
+    end
+  end
 end
--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,15 +1,14 @@
 class User::UsersController < ApplicationController
-  def unsubscribe
-    @user = User.find(current_user)
-  end
+
 
   def withdrawl
     @user = User.find(current_user.id)
     if @reported_user_id.amount > 10
       @user.update(is_active: false)
-    end
+    else
       @user.update(is_active: false)
       reset_session
       redirect_to root_path
+    end
   end
 end
