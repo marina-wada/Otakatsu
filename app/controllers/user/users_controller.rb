6638692897dc87e3f1ce7ac0f43ef1ffb68ef3cd
marina-wada <wdmrna4i1@gmail.com>
2021-10-16 09:04:08 +0000

    [Update]user_user_edit

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -2,4 +2,21 @@ class User::UsersController < ApplicationController
   def show
     @user = current_user
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :nickname, :telephone_number, :postal_code, :address, :is_active, :email, :password, :password_confirmation)
+  end
 end
