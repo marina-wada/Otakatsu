13c519457a3e12902d9349dc1ccc1f8b1a2f00bd
marina-wada <wdmrna4i1@gmail.com>
2021-10-16 15:07:15 +0000

    {Update]user_unsubscribe

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -16,6 +16,23 @@ class User::UsersController < ApplicationController
     end
   end
 
+  def unsubscribe
+    @user = User.find(current_user.id)
+  end
+
+  def check
+    @user = User.find(params[:id])
+  end
+
+  def withdrawl
+    @user = User.find(current_user.id)
+    @user.update(is_active: '退会済')
+    reset_session
+    redirect_to root_path
+  end
+
+  private
+
   def user_params
     params.require(:user).permit(:name, :nickname, :telephone_number, :postal_code, :address, :is_active, :email, :password, :password_confirmation)
   end
