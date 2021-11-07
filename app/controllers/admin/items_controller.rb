65e3588048de8a514855cc48513832a4ee34c59d
marina-wada <wdmrna4i1@gmail.com>
2021-11-07 15:12:05 +0000

    [Update]admin_user_index

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,10 @@
 class Admin::ItemsController < ApplicationController
+  def index
+    @items = Item.all
+    # @user = User.find(params[:item_user_id])
+    # @items = @user.items
+    @item = Item.find(params[:item_id])
+    @report = Report.where(reported_id: @item.item_user_id)
+    @item.report.amount = @report.amount
+  end
 end
